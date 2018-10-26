12ba1247718548b32bff5049aa1e99d8134236a4
kauaipalama <kauaipalama@gmail.com>
2018-10-26 15:56:47 -0700

    Added pinch to zoom functionality to photoDetailVC using a scrollView and its viewForZooming delegate function.

--- Sprout/View Controllers/PhotoDetailViewController.swift
+++ Sprout/View Controllers/PhotoDetailViewController.swift
@@ -10,19 +10,34 @@
 
 import UIKit
 
-class PhotoDetailViewController: UIViewController{
+class PhotoDetailViewController: UIViewController, UIScrollViewDelegate{
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        plantPhotoDetail.image = plantPhoto
+        setupViews()
     }
     
-    // MARK: - Outlet
+    // MARK: - Outlets
     
     @IBOutlet weak var plantPhotoDetail: UIImageView!
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // MARK: - Views
+    func setupViews() {
+        plantPhotoDetail.image = plantPhoto
+        plantPhotoDetail.isUserInteractionEnabled = true
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
+    }
+    
+    // MARK: - Delegate Functions
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return plantPhotoDetail
+    }
+    
     // MARK: - Property
     
     var plantPhoto: UIImage?
